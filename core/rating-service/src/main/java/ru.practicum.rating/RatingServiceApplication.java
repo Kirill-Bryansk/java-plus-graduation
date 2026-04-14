8d54871e472e34804794a22f3f91b6af7f9eedc1
Kirill-Bryansk <k.mashnov@yandex.ru>
2026-04-14 19:35:42 +1000

    Запустил все сервисы 9 ошибок postman

--- core/rating-service/src/main/java/ru.practicum.rating/RatingServiceApplication.java
+++ core/rating-service/src/main/java/ru.practicum.rating/RatingServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "ru.practicum.client")
+@EnableFeignClients(basePackages = "ru.practicum")
 public class RatingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RatingServiceApplication.class, args);
--- core/request-service/src/main/java/ru/practicum/request/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/practicum/request/RequestServiceApplication.java
@@ -3,9 +3,11 @@ package ru.practicum.request;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "ru.practicum.client")
+@ComponentScan(basePackages = "ru.practicum") // для клиента
+@EnableFeignClients(basePackages = "ru.practicum")
 public class RequestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RequestServiceApplication.class, args);
